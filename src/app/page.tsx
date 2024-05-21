88600cbb616c982d07a0dac964c665df9909347f
Sani Abdullahi <gotosannie@gmail.com>
2024-05-21 15:23:25 +0100

    updates done by me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockURLs = [
+  "https://utfs.io/f/c4ca1739-26e2-45df-a5a3-a8dd10331dae-jqkvd6.jpg",
+  "https://utfs.io/f/12ea36d0-3dd2-4e6d-8d67-87cc39308c2e-vtgf75.jpg",
+  "https://utfs.io/f/5eddbf88-01a5-48d4-9e03-14f5e853e1b2-d28i8l.jpg",
+  "https://utfs.io/f/11e6c77c-14ad-4a02-9b60-a85ec0a3f601-d2lwu2.jpg"
+];
+
+const mockImages = mockURLs.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-   Hello (gallery in progress)
+    <main className="">
+    <div className="flex flex-wrap gap-4">
+      {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} />
+        </div>
+      ))}
+      </div>
     </main>
   );
 }
